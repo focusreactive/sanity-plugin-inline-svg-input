90d3f7cf8cd493e9db6606c6d72f5fec1bbe167f
Eugene Boruhov <eugene@focusreactive.com>
2023-11-09 16:19:49 +0100

    Init

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import {definePlugin} from 'sanity'
+
+interface MyPluginConfig {
+  /* nothing here yet */
+}
+
+/**
+ * Usage in `sanity.config.ts` (or .js)
+ *
+ * ```ts
+ * import {defineConfig} from 'sanity'
+ * import {myPlugin} from 'sanity-plugin-inline-svg-input'
+ *
+ * export default defineConfig({
+ *   // ...
+ *   plugins: [myPlugin()],
+ * })
+ * ```
+ */
+export const myPlugin = definePlugin<MyPluginConfig | void>((config = {}) => {
+  // eslint-disable-next-line no-console
+  console.log('hello from sanity-plugin-inline-svg-input')
+  return {
+    name: 'sanity-plugin-inline-svg-input',
+  }
+})
